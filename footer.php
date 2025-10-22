b22e1abb6c7896aab393ca8f2ddd9e08113fd7f9
Gertinjo <gerticalaj2013@gmail.com>
2025-10-22 19:12:48 +0200

    First.

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer style="background-color:#1e3799; color:white; text-align:center; padding:40px; font-family:'Roboto', sans-serif;">
+  <h1 style="font-size:3em; margin:0;">The end</h1>
+  <p style="font-size:1.3em; margin:10px 0;">No copyright.</p>
+  <nav style="margin-top:15px;">
+  </nav>
+</footer>
+
+
+
+
+<h1> this is a footer </h1>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header style="background-color:#1e3799; color:white; text-align:center; padding:40px; font-family:'Roboto', sans-serif;">
+  <h1 style="font-size:3em; margin:0;">Digjital School</h1>
+  <p style="font-size:1.3em; margin:10px 0;">Inspiring Codes, Proggraming and Futures</p>
+  <nav style="margin-top:15px;">
+    <a href="#about" style="color:white; text-decoration:none; margin:0 20px;">About Us</a>
+    <a href="#admissions" style="color:white; text-decoration:none; margin:0 20px;">Admissions</a>
+    <a href="#programs" style="color:white; text-decoration:none; margin:0 20px;">Programs</a>
+    <a href="#contact" style="color:white; text-decoration:none; margin:0 20px;">Contact</a>
+  </nav>
+</header>    
+<h2> This is a header</h2>
