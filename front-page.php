b2c198a66b54e2c4de8cbe09f968f7ce28e1cb74
Gertinjo <gerticalaj2013@gmail.com>
2025-12-03 19:17:46 +0100

    d

--- front-page.php
+++ front-page.php
@@ -7,32 +7,33 @@ get_header() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Front-Page</title>
 </head>
-<body class="body">
-    <img src="<?php echo esc_url(get_theme_file_uri("t.PNG")) ?> " class="imazhi1">
-    <h1 class="first-header"> Model 3 </h1>
+<body class="bodyy">
+    <img src="<?php echo esc_url(get_theme_file_uri("L1-removebg-preview.PNG")) ?> " class="imazhi2">
+    <h2 class='tekstiMain'>You can't hide who you are</h2>
 
+    <img src="<?php echo esc_url(get_theme_file_uri("l.PNG")) ?> " class="imazhi1">
+
+    <img src="<?php echo esc_url(get_theme_file_uri("l-removebg-preview.PNG")) ?> " class="imazhi3">
+
+    <h2 class='tekstiMain1'>Dare to live more</h2>
+
+     <img src="<?php echo esc_url(get_theme_file_uri("l2.PNG")) ?> " class="imazhi4">
+    <section class='zh'>
+        <!-- <h2 class="textMain2"> Dealer Locator</h2> -->
+     <img src="<?php echo esc_url(get_theme_file_uri("asd.png")) ?> " class="zhi4">
+</section>
+        <section class='av'>
+    <h5 class="la"> Configure your </h5>
+    <h2 class="textMain12">Temerario</h2>
+        
+     <img src="<?php echo esc_url(get_theme_file_uri("model_chooser_temerario-removebg-preview.png")) ?> " class="imazhi4">
+
+
+</section>
     <main>
-    <section class="kerret">
-        <h2 class=first-header4> Car Models </h2>
-    <div class="modeli3">
-    <img src="<?php echo esc_url(get_theme_file_uri("t1.png")) ?>" class="imazhi2"> 
-    <h1 class="first-header3"> Model 3 </h1>
-</div>
-<div class="modeliX">
-    <img src="<?php echo esc_url(get_theme_file_uri("t3.png")) ?>" class="imazhi2"> 
-    <h1 class="first-header3"> Model X </h1>
-<div class="modeliS">
-    <img src="<?php echo esc_url(get_theme_file_uri("t4.png")) ?>" class="imazhi2"> 
-    <h1 class="first-header3"> Model S </h1>
-<div class="panell">
-    <h2 class="first-header5"> More Things We Offer </h2>
-    <img src="<?php echo esc_url(get_theme_file_uri("t5.png")) ?>" class="imazhi2"> 
-    <h1 class="first-header3"> </h1>
-</div>
-<div class="frigo">
-    <img src="<?php echo esc_url(get_theme_file_uri("t6.png")) ?>" class="imazhi2"> 
-    <h1 class="first-header3"> </h1>
-</div>
+
+
+
 </div>
 </main>
 
